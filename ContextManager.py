d3df190fecd66cf60409fb7f9c967957491374e1
I2Lue <vishnikinilya@outlook.com>
2017-04-11 08:32:16 +0300

    ContextManager

--- ContextManager.py
+++ ContextManager.py
@@ -1,20 +1,20 @@
 class ListTrakzation():
 	def __init__(self, thelist):
 		self.thelist = thelist
-		
+
 	def __enter__(self):
 		self.workingcopy = self.thelist
 		return self.workingcopy
-		
+
 	def __exit__(self, exc, value, traceback):
 		if exc is None:
 			self.thelist[:] = self.workingcopy
 		return False
-		
-items = [1,2,3]
+
+items = [1, 2, 3]
 
 with ListTrakzation(items) as working:
 	working.append(4)
 	working.append(5)
-	
+
 print(items)
